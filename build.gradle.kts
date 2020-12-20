c951e8d73a621b5b9be7a10932ea63008f2f2b35
marzelwidmer <marzelwidmer@gmail.com>
2020-12-20 14:58:34 +0100

    Save

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,17 +33,141 @@ buildscript {
 }
 
 allprojects {
+
     group = "ch.keepcalm"
+    version = "1.0.1"
+
+//    repositories {
+//        jcenter()
+//        mavenLocal()
+//        mavenCentral()
+//        maven { url = uri("https://repo.spring.io/milestone") }
+//    }
+}
+
+subprojects {
     repositories {
         mavenLocal()
         mavenCentral()
         jcenter()
         maven { url = uri("https://repo.spring.io/milestone") }
     }
+
+    println("Enabling jFrog Bintray plugin in project ${project.name}...")
+    apply(plugin = "com.jfrog.bintray")
+    println("Enabling Java plugin in project ${project.name}...")
+    apply(plugin = "java")
+    println("Enabling maven-publish plugin in project ${project.name}...")
+    apply(plugin = "maven-publish")
+    println("Enabling Kotlin Spring plugin in project ${project.name}...")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    tasks.withType<KotlinCompile> {
+        println("Configuring KotlinCompile  $name in project ${project.name}...")
+        kotlinOptions {
+            languageVersion = "1.4"
+            apiVersion = "1.4"
+            jvmTarget = "11"
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+        }
+    }
+    java {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    tasks {
+        val sourcesJar by creating(Jar::class) {
+            dependsOn(JavaPlugin.CLASSES_TASK_NAME)
+            archiveClassifier.set("sources")
+            from(sourceSets["main"].allSource)
+        }
+        val javadocJar by creating(Jar::class) {
+            dependsOn(JavaPlugin.JAVADOC_TASK_NAME)
+            archiveClassifier.set("javadoc")
+            from(javadoc)
+        }
+        artifacts {
+            archives(sourcesJar)
+            archives(javadocJar)
+            archives(jar)
+        }
+    }
+    tasks.withType<Javadoc>() {
+        if (JavaVersion.current().isJava8Compatible) {
+            (options as StandardJavadocDocletOptions).addStringOption("Xdoclint:none", "-quiet")
+        }
+        if (JavaVersion.current().isJava9Compatible) {
+            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+        }
+    }
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    publishing {
+        publications {
+            println("--------> ${project.name}")
+            create<MavenPublication>("${project.name}") {
+                from(components["java"])
+                artifact(tasks["sourcesJar"])
+                artifact(tasks["javadocJar"])
+
+                pom {
+                    name.set("Spring Boot Starter")
+                    description.set("A Spring Boot Starter.")
+                    url.set("https://github.com/marzelwidmer?tab=repositories")
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("marzelwidmer")
+                            name.set("Marcel Widmer")
+                            email.set("marzelwidmer@gmail.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:git://github.com:marzelwidmer/${project.name}.git")
+                        developerConnection.set("https://github.com/marzelwidmer/${project.name}.git")
+                        url.set("http://blog.marcelwidmer.org")
+                    }
+                }
+            }
+        }
+    }
+
+    bintray {
+        user = if (project.hasProperty("bintrayUser")) project.property("bintrayUser") as String? else System.getenv("BINTRAY_USER")
+        key = if (project.hasProperty("bintrayApiKey")) project.property("bintrayApiKey") as String? else System.getenv("BINTRAY_API_KEY")
+        publish = true
+        setPublications(project.name)
+
+        pkg.apply {
+            //   https://dl.bintray.com/marzelwidmer/myrepository
+            repo = "myrepository" //'<repository name on bintray>'
+            name = "${project.name}" //'<artifactId>'
+            version.apply {
+                name = "${project.version}"
+                released = org.apache.tools.ant.types.resources.selectors.Date().toString()
+                vcsTag = "v${project.version}"
+            }
+        }
+    }
 }
 
-group = "ch.keepcalm"
-version = "1.0.0"
+
+tasks.named("afterReleaseBuild") {
+    dependsOn(
+        listOf(
+            ":kboot-spring-boot-starter-autoconfigure:publish",
+            ":kboot-spring-boot-starter-autoconfigure-starter:publish"
+        )
+    )
+}
+
 
 //val artifactName = project.name
 //val artifactGroup = project.group.toString()
@@ -96,214 +220,3 @@ version = "1.0.0"
 //        }
 //    }
 //}
-
-subprojects {
-    println("Enabling Java plugin in project ${project.name}...")
-    apply(plugin = "java")
-    println("Enabling maven-publish plugin in project ${project.name}...")
-    apply(plugin = "maven-publish")
-    println("Enabling Kotlin Spring plugin in project ${project.name}...")
-    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
-    tasks.withType<KotlinCompile> {
-        println("Configuring KotlinCompile  $name in project ${project.name}...")
-        kotlinOptions {
-            languageVersion = "1.4"
-            apiVersion = "1.4"
-            jvmTarget = "11"
-            freeCompilerArgs = listOf("-Xjsr305=strict")
-        }
-    }
-    java {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    tasks {
-        val sourcesJar by creating(Jar::class) {
-            dependsOn(JavaPlugin.CLASSES_TASK_NAME)
-            archiveClassifier.set("sources")
-            from(sourceSets["main"].allSource)
-        }
-        val javadocJar by creating(Jar::class) {
-            dependsOn(JavaPlugin.JAVADOC_TASK_NAME)
-            archiveClassifier.set("javadoc")
-            from(javadoc)
-        }
-        artifacts {
-            archives(sourcesJar)
-            archives(javadocJar)
-            archives(jar)
-        }
-    }
-    tasks.withType<Test> {
-        useJUnitPlatform()
-    }
-    tasks.withType<Javadoc>() {
-        if (JavaVersion.current().isJava8Compatible) {
-            (options as StandardJavadocDocletOptions).addStringOption("Xdoclint:none", "-quiet")
-        }
-        if (JavaVersion.current().isJava9Compatible) {
-            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-        }
-    }
-//    publishing {
-//        publications {
-//            create<MavenPublication>("kboot-spring-boot-starter") {
-//                from(components["java"])
-//                artifact(tasks["sourcesJar"])
-//                artifact(tasks["javadocJar"])
-//
-//                pom {
-//                    name.set("Spring Boot Starter")
-//                    description.set("A Spring Boot Starter.")
-//                    url.set("https://github.com/marzelwidmer?tab=repositories")
-//
-//                    licenses {
-//                        license {
-//                            name.set("The Apache License, Version 2.0")
-//                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//                        }
-//                    }
-//                    developers {
-//                        developer {
-//                            id.set("marzelwidmer")
-//                            name.set("Marcel Widmer")
-//                            email.set("marzelwidmer@gmail.com")
-//                        }
-//                    }
-//                    scm {
-//                        connection.set("scm:git:git://github.com:marzelwidmer/${project.name}.git")
-//                        developerConnection.set("https://github.com/marzelwidmer/${project.name}.git")
-//                        url.set("http://blog.marcelwidmer.org")
-//                    }
-//                }
-//            }
-//        }
-//    }
-}
-
-bintray {
-    user = if (project.hasProperty("bintrayUser")) project.property("bintrayUser") as String? else System.getenv("BINTRAY_USER")
-    key = if (project.hasProperty("bintrayApiKey")) project.property("bintrayApiKey") as String? else System.getenv("BINTRAY_API_KEY")
-    publish = true
-    setPublications(project.name)
-
-    pkg.apply {
-        //   https://dl.bintray.com/marzelwidmer/myrepository
-        repo = "myrepository" //'<repository name on bintray>'
-        name = "${project.name}" //'<artifactId>'
-        version.apply {
-            name = "${project.version}"
-            released = org.apache.tools.ant.types.resources.selectors.Date().toString()
-            vcsTag = "v${project.version}"
-        }
-    }
-    publishing {
-        publications {
-            create<MavenPublication>("${project.name}") {
-                from(components["java"])
-
-                pom {
-                    name.set("Spring Boot Starter")
-                    description.set("A Spring Boot Starter.")
-                    url.set("https://github.com/marzelwidmer?tab=repositories")
-
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("marzelwidmer")
-                            name.set("Marcel Widmer")
-                            email.set("marzelwidmer@gmail.com")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:git://github.com:marzelwidmer/${project.name}.git")
-                        developerConnection.set("https://github.com/marzelwidmer/${project.name}.git")
-                        url.set("http://blog.marcelwidmer.org")
-                    }
-                }
-            }
-        }
-    }
-}
-
-tasks.named("afterReleaseBuild") {
-    dependsOn(
-        listOf(
-            ":kboot-spring-boot-starter-autoconfigure:publish",
-            ":kboot-spring-boot-starter-autoconfigure-starter:publish"
-        )
-    )
-}
-
-//
-//bintray {
-//    user = '<bintray user name>'
-//    key = '<bintray API key>'
-//    publications = ['mavenJava']
-//
-//    pkg {
-//        repo = '<repository name on bintray>'
-//        name = '<artifactId>'
-//
-//        version {
-//            name = project.version
-//            released = new Date()
-//            vcsTag = "v${project.version}"
-//        }
-//    }
-//}
-//
-//task sourcesJar(type: Jar, dependsOn: project.classes) {
-//    from sourceSets.main.allSource
-//}
-//
-//task javadocJar(type: Jar, dependsOn: project.javadoc) {
-//    from javadoc.destinationDir
-//}
-//
-//artifacts {
-//    archives sourcesJar, javadocJar
-//}
-//
-//publishing {
-//    publications {
-//        mavenJava(MavenPublication) {
-//            artifactId project.bintray.pkg.name
-//                    from components.java
-//
-//                    artifact sourcesJar {
-//                classifier = 'sources'
-//            }
-//            artifact javadocJar {
-//                classifier = 'javadoc'
-//            }
-//        }
-//    }
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
